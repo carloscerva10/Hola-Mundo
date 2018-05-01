c5fe14502cd1e03768165d79f7a3a55b5645e160
Cervantes Aguirre <carlosalberto.cervantesaguirre@cgi.com>
2018-05-01 12:56:23 +0200

    cambio de titulo

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
-		<h1>CLIENTE</h1>
+		<h1>CLIENTE CARLOS</h1>
 		Ingrese alguno de los siguientes productos: <strong>zapatos, pantalones, camisa</strong>
 		<hr>
 		<form method="post" action="">
